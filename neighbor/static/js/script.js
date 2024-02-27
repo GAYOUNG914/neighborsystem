5a5973d0ec6ddbf35c706554c72a3b340d200dcf
rowanrowan12 <rowan@fave.kr>
2024-02-27 18:07:54 +0900

    commit

--- neighbor/static/js/script.js
+++ neighbor/static/js/script.js
@@ -1,195 +1,105 @@
 // // import '../themes/style.scss';
 
 ////carousel
-
-let isDragging = false;
-let startX, scrollLeft, percent;
+const prevButton = document.querySelector('.btn-prv');
+const nextButton = document.querySelector('.btn-next');
+const $currentIndex = document.querySelector('.current-index');
+const $slidesLength = document.querySelector('.slides-length');
 const slider = document.querySelector('.carousel');
 const sliderTrack = document.querySelector('.carousel-track');
-const slides = document.querySelectorAll('.crousel-slide');
-const lastIndex = slides.length - 1;
-let selected = 0;
+let slides = document.querySelectorAll('.crousel-slide');
+let currentIndex = 0;
 let interval;
 
+gsap.to( slider, {y:-150, opacity: 1, delay: 1.85, duration: 1.4, ease: "expo.out" });
 
-// sliderTrack.addEventListener('mousedown', (e) => {
-//   isDragging = true;
-//   startX = e.pageX - sliderTrack.offsetLeft; //부모로부터 좌로 얼마나 떨어져 있는지
-//   // scrollLeft = sliderTrack.scrollLeft;
 
-//   console.log('down')
-// });
+function initSlideIndex(){
+  $slidesLength.textContent = '/' + slides.length;
 
-// sliderTrack.addEventListener('mouseup', () => {
-//   isDragging = false;
-
-//   console.log('up')
-
-//   // setSliderState(percent);
-// });
-
-// sliderTrack.addEventListener('mousemove', (e) => {
-//   if (!isDragging) return;
-//   e.preventDefault();
-//   const x = e.pageX - sliderTrack.offsetLeft;
-//   const walk = x - startX;
-//   // sliderTrack.scrollLeft = scrollLeft - walk;
-
-//   percent = walk/startX * 100;
-
-//   // setSliderCSS(percent);
-
-//   console.log('move')
-// });
-
-function initSlideState (){
-
-  for(let i=0; i < slides.length; i++){
+  for (let i = 0; i < slides.length; i++) {
+    
     slides[i].setAttribute('data-index',i);
+
+    if(i > slides.length/2){
+        slides[i].setAttribute('data-index',Math.floor(slides.length/2) - i);
+    }
+  }
+  for (let i = 0; i < slides.length; i++) {
+    if(slides[i].getAttribute('data-index') < 0){
+      const firstChild = sliderTrack.firstElementChild;
+      sliderTrack.insertBefore(slides[i], firstChild);
+    }
+  }
+}
+
+function moveSlide(num){
+
+  currentIndex = num;
+  
+  $currentIndex.textContent = num + 1;
+  if(num + 1 <= 0){
+    num = slides.length + num
+    $currentIndex.textContent = num + 1;
   }
 
   for(let a of slides){
-    // if(!a.classList.contains('crousel-slide--active')){
+    a.classList.remove('active');
+    a.classList.remove('prev');
+    a.classList.remove('next');
 
-      a.classList.remove('crousel-slide--active');
-      // console.log(a.getAttribute('data-index'))
-      if(a.getAttribute('data-index') == selected)a.classList.add('crousel-slide--active');
-    // }
+    if(currentIndex > slides.length/2){
+      currentIndex = currentIndex - slides.length
+    }
+
+    if(a.getAttribute('data-index') == currentIndex){
+      a.classList.add('active')
+    }else if(a.getAttribute('data-index') == currentIndex - 1){
+      a.classList.add('prev')
+    }else if(a.getAttribute('data-index') == currentIndex + 1){
+      a.classList.add('next')
+    }
+
+    //끝 인덱스 분기처리
+    if(currentIndex + 1 > slides.length/2){
+      a.classList.remove('next');
+      if(a.getAttribute('data-index') == currentIndex - (slides.length - 1)){
+        a.classList.add('next');
+      }
+    }else if(currentIndex - 1 < -1 * Math.floor(slides.length/2) && slides.length % 2 !== 0){
+      //slides.length 홀수
+      a.classList.remove('prev');
+      if(a.getAttribute('data-index') == -currentIndex){
+        a.classList.add('prev');
+      }
+    }else if(currentIndex - 1 < -1 * Math.floor(slides.length/2) + 1 && slides.length % 2 == 0){
+      //slides.length 짝수
+      a.classList.remove('prev');
+      if(a.getAttribute('data-index') == Math.floor(slides.length + currentIndex - 1)){
+        a.classList.add('prev');
+      }
+    }
   }
-  console.log(selected)
-  
-
-  const activeIndex = Array.from(slides).findIndex(element => element.classList.contains('crousel-slide--active'));
-  
+  // console.log(-1 * Math.floor(slides.length/2))
 }
 
 
-//슬라이드 전환 시 CSS Animation 속성을 부여
-const setTransition = (value) => {
-  sliderTrack.style.transition = value;
-};
+prevButton.addEventListener('click',()=>{
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  moveSlide(currentIndex);
+})
 
-//슬라이드 인덱스에 맞춰 CSS Translate 값을 조정
-const setTranslate = ({ index, reset }) => {
-  if (reset) sliderTrack.style.transform = `translate(-${sliderTrack.clientWidth/slides.length}px, 0)`;
-  else 
-  sliderTrack.style.transform = `translate(-${(index + 1) * sliderTrack.clientWidth/slides.length}px, 0)`;
-};
+nextButton.addEventListener('click',()=>{
+  currentIndex = (currentIndex + 1) % slides.length;
+  moveSlide(currentIndex);
+})
 
-const handlePrev = () => {
-  selected -= 1;
-  setTransition('transform 0.3s linear');
-  if (selected < 0) {
-    selected = 0;
-    slides[lastIndex].remove();
-    slides[lastIndex-1].remove();
+initSlideIndex();
+moveSlide(currentIndex);
 
-
-    initSlideState();
-    setTranslate({ index: selected });
-    selected = lastIndex;
-
-  }
-};
-
-const handleNext = () => {
-  selected += 1;
-  setTransition('transform 0.3s linear');
-  setTranslate({ index: selected });
-  if (selected > lastIndex) {
-    selected = 0;
-
-  }
-  initSlideState();
-  // console.log(selected);
-};
-
-const makeButton = () => {
-  if (slides.length > 1) {
-    const prevButton = document.createElement('button');
-    prevButton.classList.add('btn');
-    prevButton.classList.add('btn-prv');
-    // prevButton.innerHTML = '<div class="carousel-btns"></div>';
-    prevButton.addEventListener('click', handlePrev);
-
-    const nextButton = document.createElement('button');
-    nextButton.classList.add('btn');
-    nextButton.classList.add('btn-next');
-    // nextButton.innerHTML = '<div class="carousel-btns"></div>';
-    nextButton.addEventListener('click', handleNext);
-
-    document.querySelector('.carousel-btns').appendChild(prevButton);
-    document.querySelector('.carousel-btns').appendChild(nextButton);
-  }
-};
-
-
-const cloneElement = () => {
-  sliderTrack.prepend(slides[lastIndex].cloneNode(true));
-  sliderTrack.prepend(slides[lastIndex - 1].cloneNode(true));
-  // for(let i=slides.length - 1; i > -1 ; i--){
-  //   sliderTrack.prepend(slides[i].cloneNode(true));
-  // }
-  // sliderTrack.append(slides[1].cloneNode(true));
-  setTranslate({ reset: true });
-};
-
-const render = () => {
-  initSlideState();
-  makeButton();
-  cloneElement();
-  // autoplay({ duration: 2000 });
-};
-render();
-
-
-
-// function setSliderState(percent){
-
-  // const activeSlide = document.querySelector('.crousel-slide--active');
-  // const prevSlide = document.querySelector('.crousel-slide--prev');
-  // const prevprevSlide = document.querySelector('.crousel-slide--prev--prev');
-  // const nextSlide = document.querySelector('.crousel-slide--next');
-  // const nextnextSlide = document.querySelector('.crousel-slide--next--next');
-
-  // const prevprevIndex = Array.from(slides).findIndex(element => element.classList.contains('crousel-slide--prev--prev'));
-  // const nextnextIndex = Array.from(slides).findIndex(element => element.classList.contains('crousel-slide--next--next'));
-
-  // if (percent < 0 && slides.length > 5){
-  //   //좌로 드래그, 슬라이드 갯수 5개보다 많음
-  
-  //   //prevprev가 안보여야됨
-  //   activeSlide.setAttribute('class','crousel-slide--prev');
-  //   prevSlide.setAttribute('class','crousel-slide--prev--prev');
-  //   prevprevSlide.setAttribute('class','crousel-slide');
-  //   nextSlide.setAttribute('class','crousel-slide--active');
-  //   nextnextSlide.setAttribute('class','crousel-slide--next');
-  //   //nextnext다음의 슬라이드에 nextnext 클래스 붙어야함
-  //   //nextnext다음의 슬라이드 인덱스 들고와서 클래스 붙이면 될듯
-  //   slides[nextnextIndex + 1].setAttribute('class','crousel-slide--next--next');
-  // }else if(percent < 0 && slides.length == 5){
-  //   //좌로 드래그, 슬라이드 갯수가 5개
-  //   //이 땐 어떻게 할겨..?
-  // }else if(percent < 0 && slides.length < 5){
-  //   //좌로 드래그, 슬라이드 갯수가 5개 미만
-  //   //이 땐 어떻게 할겨..?
-  // }
-
-  // if(percent > 0 && slides.length > 5){
-  //   //우로 드래그, 슬라이드 갯수 5개보다 많음
-    
-  //   //nextnext가 안보여야됨
-  //   activeSlide.setAttribute('class','crousel-slide--next');
-  //   prevSlide.setAttribute('class','crousel-slide--active');
-  //   prevprevSlide.setAttribute('class','crousel-slide--prev');
-  //   nextSlide.setAttribute('class','crousel-slide--next--next');
-  //   nextnextSlide.setAttribute('class','crousel-slide');
-  //   //prevprev이전의 슬라이드에 prevprev 클래스 붙어야함
-  //   //prevprev이전의  슬라이드 인덱스 들고와서 클래스 붙이면 될듯    
-  //   slides[prevprevIndex - 1].setAttribute('class','crousel-slide--prev--prev');
-  // }
-
-// }
-
-gsap.to( slider, { y: -150, opacity: 1, scale: 1, delay: 1.85, duration: 1.4, ease: "expo.out" })
+//드래그 슬라이드
+//api 클릭 이벤트 (노션 참고)
+//보드판 나오게 작업
+//마법진 안 잘리게 + 다듬기 (a : 실린더로 빛 효과)
+//플러그인화(스피드,딜레이,액티브 슬라이드 정보 콘솔찍기)
 
