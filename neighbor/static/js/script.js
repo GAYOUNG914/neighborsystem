90e1c716f6f1c5035dc0250fb3d3f741fc2b9a1b
rowanrowan12 <rowan@fave.kr>
2024-03-05 18:15:30 +0900

    commit

--- neighbor/static/js/script.js
+++ neighbor/static/js/script.js
@@ -5,6 +5,7 @@ export default function Carousel(domElem, option){
 
   const prevButton = document.querySelector('.btn-prv');
   const nextButton = document.querySelector('.btn-next');
+  const $autoplayButton = document.querySelector('.btn.play');
   const $currentIndex = document.querySelector('.current-index');
   const $slidesLength = document.querySelector('.slides-length');
   const $slider = typeof domElem == 'string' ? document.querySelector(`${domElem}`) : domElem;
@@ -12,21 +13,32 @@ export default function Carousel(domElem, option){
   const $count = document.querySelector('.count');
   const $countprevBtn = document.querySelector('.carousel-btns .btn-prv');
   const $countnextBtn = document.querySelector('.carousel-btns .btn-next');
+  const $countplayBtn = document.querySelector('.carousel-btns .play');
   let slides = document.querySelectorAll('.crousel-slide');
   let currentIndex = 0;
-  option = {};
+  let isPlaying = false;
+  let interval;
 
-  option.speed = option.speed;
+  option.speed = option.speed || 0.3 ;
   option.delay = option.delay;
   // option.lightSpeed = option.lightSpeed;
   // option.lightColor = option.lightColor;
-  // option.onChangeActive = option.onChangeActive;
+  option.onChangeActive = option.onChangeActive;
 
   gsap.to( $slider, {y: -150, opacity: 1, delay: 1.85, duration: 1.4, ease: "expo.out" });
-  // gsap.to( $count, {x: -400, opacity: 1, delay: 2.2, duration: 1.4, ease: "expo.out" });
   gsap.to( $count, {opacity: 1, delay: 2.45, duration: 0.5, ease: "expo.out" });
   gsap.to( $countprevBtn, {x: -60, opacity: 1, delay: 2.3, duration: 0.5, ease: "expo.inout" });
   gsap.to( $countnextBtn, {x: 60, opacity: 1, delay: 2.3, duration: 0.5, ease: "expo.inout" });
+  gsap.to( $countplayBtn, {x: 70, opacity: 1, delay: 2.3, duration: 0.5, ease: "expo.inout" });
+
+  
+  // for(let a of document.querySelectorAll('picture')){
+  //   a.remove();
+  //   for(let b of slides){
+  //     b.style.backgroundColor = "white";
+  //     b.style.borderRadius = '10px';
+  //   }
+  // }
 
 
   function initSlideIndex(){
@@ -129,22 +141,7 @@ export default function Carousel(domElem, option){
     }
 
     setTranslation();
-
   }
-
-
-  prevButton.addEventListener('click',()=>{
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    moveSlide(currentIndex);
-  })
-
-  nextButton.addEventListener('click',()=>{
-    currentIndex = (currentIndex + 1) % slides.length;
-    moveSlide(currentIndex);
-  })
-
-  initSlideIndex();
-  moveSlide(currentIndex);
   
   //마법진 안 잘리게 + 다듬기 (a : 실린더로 빛 효과)
   //canvas height 1.5 배
@@ -161,7 +158,7 @@ export default function Carousel(domElem, option){
   let isDragging = false;
   let startX, scrollLeft, percent, endX;
 
-  $slider.addEventListener('mousedown', (e) => {
+  function onMousedown(e){
     isDragging = true;
     // startX = e.pageX;
     startX = e.pageX - $sliderTrack.offsetLeft; //처음 좌표: 부모로부터 좌로 얼마나 떨어져 있는지
@@ -169,38 +166,35 @@ export default function Carousel(domElem, option){
     for(let a of slides){
       a.style.transition = 0 + 's';
     }
-
     console.log('down')
+    // $slider.removeEventListener('mousedown', (e) => onMousedown(e));
+  }
 
-  });
-
-  $slider.addEventListener('mouseleave', () => {
+  function onMouseleave(){
     isDragging = false;
 
     for(let a of slides){
-      a.style.transition = 0.3 + 's';
+      a.style.transition = option.speed + 's';
     }
 
     setTranslation();
-    
     console.log('leave')
-  });
+    // $slider.removeEventListener('mouseleave',onMouseleave);
+  }
 
-
-  $slider.addEventListener('mouseup', () => {
+  function onMouseup(){
     isDragging = false;
 
     for(let a of slides){
-      a.style.transition = 0.3 + 's';
+      a.style.transition = option.speed + 's';
     }
 
     judgeSlides(percent);
     console.log('up')
+    // $slider.removeEventListener('mouseup',onMouseup);
+  }
 
-  });
-
-
-  $slider.addEventListener('mousemove', (e) => {
+  function onMousemove(e){
     if (!isDragging) return;
     e.preventDefault();
     // const x = e.pageX;
@@ -208,19 +202,28 @@ export default function Carousel(domElem, option){
     const walk = x - startX;//이동한 거리 = 실시간 좌표 - 처음 좌표
 
     percent = walk/5;
-    // percent = walk/startX * 100;
-    //아 시작점이 왼쪽(숫자가 작음) -> percent 값이 커짐
-    //시작점이 오른쪽(숫자 큼) -> percent 값 작아짐
 
     giveTranslation(percent);
-    
     console.log('move')
-  });
+    // $slider.removeEventListener('mousemove', (e) => onMousemove(e));
+  }
 
-  // function onMousemove(){
-  //   // removeEvent
-  // }
+  function onPrevButton(){
+    console.log(currentIndex, 'gottenIndex')
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    console.log(currentIndex, 'realIndex')
 
+    moveSlide(currentIndex);
+  }
+  
+  function onNextButton(){
+    console.log(currentIndex, 'gottenIndex')
+    currentIndex = (currentIndex + 1) % slides.length;
+    console.log(currentIndex, 'realIndex')
+    console.log(currentIndex + 1, 'showedIndex')
+
+    moveSlide(currentIndex);
+  }
 
   function judgeSlides(percent) {
     //어느 정도 슬라이드 되면 인덱스가 넘어가는 함수
@@ -307,5 +310,47 @@ export default function Carousel(domElem, option){
     
   }
 
+  function autoPlay(){
+    currentIndex = currentIndex + 1;
+    moveSlide(currentIndex);
+  }
+
+  function onAutoplayButton(){
+
+    if(!isPlaying){
+      //stop 상태라면
+      for(let a of slides){
+        a.style.transition = option.speed + 's';
+      }  
+      $autoplayButton.classList.add('play');
+      $autoplayButton.classList.remove('stop');
+      interval = setInterval(autoPlay,1200);
+    }else{
+      //play 상태라면
+      for(let a of slides){
+        a.style.transition = 0 + 's';
+      }  
+      $autoplayButton.classList.add('stop');
+      $autoplayButton.classList.remove('play');
+      clearInterval(interval);
+    }
+
+    isPlaying = !isPlaying;
+
+  }
+  
+  setTimeout(onAutoplayButton,1700);
+  initSlideIndex();
+  moveSlide(currentIndex);
   setTranslation();
+
+
+  prevButton.addEventListener('click',onPrevButton)
+  nextButton.addEventListener('click',onNextButton)
+  $autoplayButton.addEventListener('click',onAutoplayButton);
+  
+  $slider.addEventListener('mousedown', (e) => onMousedown(e));
+  $slider.addEventListener('mouseleave',onMouseleave);
+  $slider.addEventListener('mouseup',onMouseup);
+  $slider.addEventListener('mousemove', (e) => onMousemove(e));
 }
