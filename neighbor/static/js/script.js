7065b3138bfd20af24beaaddd966c065d158bb40
rowanrowan12 <rowan@fave.kr>
2024-03-07 18:04:08 +0900

    commit

--- neighbor/static/js/script.js
+++ neighbor/static/js/script.js
@@ -9,16 +9,20 @@ export default function Carousel(domElem, option){
   const $slidesLength = document.querySelector('.slides-length');
   const $slider = typeof domElem == 'string' ? document.querySelector(`${domElem}`) : domElem;
   const $sliderTrack = document.querySelector('.carousel-track');
+  const $robotImgs = document.querySelectorAll('.carousel .img');
   const $count = document.querySelector('.count');
   const $countprevBtn = document.querySelector('.carousel-btns .btn-prv');
   const $countnextBtn = document.querySelector('.carousel-btns .btn-next');
   const $countplayBtn = document.querySelector('.carousel-btns .play');
+  const $boards = document.querySelectorAll('.board-item');
+  const $boardListsRight = document.querySelectorAll('.board-item.right .item');
+  const $boardListsLeft = document.querySelectorAll('.board-item.left .item');
   const $magiccircle = document.querySelector('.magiccircle');
   let slides = document.querySelectorAll('.crousel-slide');
   let currentIndex = 0;
   let isPlaying = false;
   let windowHasFocus = true;
-  let interval;
+  let interval, pregress;
 
 
   option.speed = option.speed || 0.3 ;
@@ -27,21 +31,33 @@ export default function Carousel(domElem, option){
   // option.lightColor = option.lightColor;
   option.onChangeActive = option.onChangeActive;
 
+
+
   gsap.to( $magiccircle, {width: 1220, opacity: 1, delay: 1.8, duration: 1.4, ease: "expo.out" });
   gsap.to( $slider, {y: -150, opacity: 1, delay: 1.85, duration: 1.4, ease: "expo.out" });
   gsap.to( $count, {opacity: 1, delay: 2.45, duration: 0.5, ease: "expo.out" });
   gsap.to( $countprevBtn, {x: -60, opacity: 1, delay: 2.3, duration: 0.5, ease: "expo.inout" });
   gsap.to( $countnextBtn, {x: 60, opacity: 1, delay: 2.3, duration: 0.5, ease: "expo.inout" });
   gsap.to( $countplayBtn, {x: 70, opacity: 1, delay: 2.3, duration: 0.5, ease: "expo.inout" });
+  gsap.to( $boards, {height: 700, delay: 2.7, duration: 1, ease: "expo.inout", opacity: 0.7 });
 
-  
-  // for(let a of document.querySelectorAll('picture')){
-  //   a.remove();
-  //   for(let b of slides){
-  //     b.style.backgroundColor = "white";
-  //     b.style.borderRadius = '10px';
+  // setTimeout(()=>{
+  //   for(let i = 0; i < $boardListsRight.length; i++){
+  //     $boardListsRight[i].style.transition = `0.2s`;
+  //     $boardListsRight[i].style.transitionDelay = `0.${i}s`;
+  //     $boardListsRight[i].style.transform = `transform: rotateX(180deg)`;
+
   //   }
-  // }
+  // },3500)
+
+  // setTimeout(()=>{
+  //   for(let i = 0; i < $boardListsLeft.length; i++){
+  //     $boardListsLeft[i].style.transition = `0.2s`;
+  //     $boardListsLeft[i].style.transitionDelay = `0.${i}s`;
+  //     $boardListsLeft[i].style.transform = `transform: rotateX(180deg)`;
+
+  //   }
+  // },3500)
 
 
   function initSlideIndex(){
@@ -168,8 +184,11 @@ export default function Carousel(domElem, option){
     for(let a of slides){
       a.style.transition = 0 + 's';
     }
+
+    isPlaying = true;
+    onAutoplayButton();
+
     console.log('down')
-    // $slider.removeEventListener('mousedown', (e) => onMousedown(e));
   }
 
   function onMouseleave(){
@@ -181,7 +200,6 @@ export default function Carousel(domElem, option){
 
     setTranslation();
     console.log('leave')
-    // $slider.removeEventListener('mouseleave',onMouseleave);
   }
 
   function onMouseup(){
@@ -211,6 +229,12 @@ export default function Carousel(domElem, option){
   }
 
   function onPrevButton(){
+    isPlaying = true;
+    onAutoplayButton();
+    for(let a of slides){
+      a.style.transition = option.speed + 's';
+    }  
+
     console.log(currentIndex, 'gottenIndex')
     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
     console.log(currentIndex, 'realIndex')
@@ -219,6 +243,12 @@ export default function Carousel(domElem, option){
   }
   
   function onNextButton(){
+    isPlaying = true;
+    onAutoplayButton();
+    for(let a of slides){
+      a.style.transition = option.speed + 's';
+    }  
+
     console.log(currentIndex, 'gottenIndex')
     currentIndex = (currentIndex + 1) % slides.length;
     console.log(currentIndex, 'realIndex')
@@ -240,25 +270,29 @@ export default function Carousel(domElem, option){
     }
   }
 
+  function pregressFunc(currentVal,tobeVal,progress){
+    return currentVal + (tobeVal - currentVal) * progress;
+  }
+
   function giveTranslation(percent){
     const $active = document.querySelector('.crousel-slide.active');
     const $prev = document.querySelector('.crousel-slide.prev');
     const $prevprev = document.querySelector('.crousel-slide.prevprev');
     const $next = document.querySelector('.crousel-slide.next');
     const $nextnext = document.querySelector('.crousel-slide.nextnext');
-    let pregress;
 
     pregress = Math.abs(percent/100);
 
-    function pregressFunc(currentVal,tobeVal,progress){
-      return currentVal + (tobeVal - currentVal) * progress;
-    }
+    // function pregressFunc(currentVal,tobeVal,progress){
+    //   return currentVal + (tobeVal - currentVal) * progress;
+    // }
 
     if(percent < 0){
       // active -> prev
       $active.style.transform = `translate3d(${pregressFunc(0,-170,pregress)}px, ${pregressFunc(0,70,pregress)}px, ${pregressFunc(0,-100,pregress)}px) rotateY(${pregressFunc(0,20,pregress)}deg) scale(${pregressFunc(1.3,0.9,pregress)})`;
       $active.style.opacity = `${pregressFunc(1,0.4,pregress)}`;
       $active.style.zIndex = `${Math.floor(pregressFunc(10,0,pregress*2))}`; 
+      $active.style.filter = `blur(${pregressFunc(0,5,pregress)}px)`;
       // prev -> prevprev
       $prev.style.transform = `translate3d(${pregressFunc(-170,-120,pregress)}px, ${pregressFunc(70,100,pregress)}px, ${pregressFunc(-100,-110,pregress)}px) rotateY(${pregressFunc(20,20,pregress)}deg) scale(${pregressFunc(0.9,0.7,pregress)})`;
       $prev.style.opacity = `${pregressFunc(0.4,0,pregress)}`;
@@ -266,6 +300,7 @@ export default function Carousel(domElem, option){
       $next.style.transform = `translate3d(${pregressFunc(170,0,pregress)}px, ${pregressFunc(70,0,pregress)}px, ${pregressFunc(-100,0,pregress)}px) rotateY(${pregressFunc(-20,0,pregress)}deg) scale(${pregressFunc(0.9,1.35,pregress)})`;
       $next.style.opacity = `${pregressFunc(0.4,1,pregress)}`;
       $next.style.zIndex = `${Math.floor(pregressFunc(0,10,pregress*2))}`; 
+      $next.style.filter = `blur(${pregressFunc(5,0,pregress)}px)`;
       // nextnext -> next
       $nextnext.style.transform = `translate3d(${pregressFunc(120,170,pregress)}px, ${pregressFunc(100,70,pregress)}px, ${pregressFunc(-110,-100,pregress)}px) rotateY(${pregressFunc(-20,-20,pregress)}deg) scale(${pregressFunc(0.7,0.9,pregress)})`;
       $nextnext.style.opacity = `${pregressFunc(0,0.4,pregress)}`;
@@ -275,9 +310,11 @@ export default function Carousel(domElem, option){
       $active.style.transform = `translate3d(${pregressFunc(0,170,pregress)}px, ${pregressFunc(0,70,pregress)}px, ${pregressFunc(0,-100,pregress)}px) rotateY(${pregressFunc(0,-20,pregress)}deg) scale(${pregressFunc(1.3,0.9,pregress)})`;
       $active.style.opacity = `${pregressFunc(1,0.4,pregress)}`;
       $active.style.zIndex = `${Math.floor(pregressFunc(10,0,pregress*2))}`; 
+      $active.style.filter = `blur(${pregressFunc(0,5,pregress)}px)`;
       // prev -> active
       $prev.style.transform = `translate3d(${pregressFunc(-170,0,pregress)}px, ${pregressFunc(70,0,pregress)}px, ${pregressFunc(-100,0,pregress)}px) rotateY(${pregressFunc(20,0,pregress)}deg) scale(${pregressFunc(0.9,1.35,pregress)})`;
       $prev.style.opacity = `${pregressFunc(0.4,1,pregress)}`;
+      $prev.style.filter = `blur(${pregressFunc(5,0,pregress)}px)`;
       // prevprev -> prev
       $prevprev.style.transform = `translate3d(${pregressFunc(-120,-170,pregress)}px, ${pregressFunc(100,70,pregress)}px, ${pregressFunc(-110,-100,pregress)}px) rotateY(${pregressFunc(20,20,pregress)}deg) scale(${pregressFunc(0.7,0.9,pregress)})`;
       $prevprev.style.opacity = `${pregressFunc(0,0.4,pregress)}`;
@@ -297,19 +334,23 @@ export default function Carousel(domElem, option){
     $active.style.transform = `translate3d(0, 0, 0) rotateY(0) scale(1.35)`;
     $active.style.opacity = `1.3`
     $active.style.zIndex = `10`
+    $active.style.filter = `none`
     $prev.style.transform = `translate3d(-170px, 70px, -100px) rotateY(20deg) scale(0.9)`;
     $prev.style.opacity = `0.4`
     $prev.style.zIndex = `0`
+    $prev.style.filter = `blur(5px)`
     $prevprev.style.transform = `translate3d(0, 0, 0) rotateY(0) scale(0.7)`;
     $prevprev.style.opacity = `0`
     $prevprev.style.zIndex = `0`
+    $prevprev.style.filter = `blur(5px)`
     $next.style.transform = `translate3d(170px, 70px, -100px) rotateY(-20deg) scale(0.9)`;
     $next.style.opacity = `0.4`
     $next.style.zIndex = `0`
+    $next.style.filter = `blur(5px)`
     $nextnext.style.transform = `translate3d(0, 0, 0) rotateY(0) scale(0.7)`;
     $nextnext.style.opacity = `0`
     $nextnext.style.zIndex = `0`
-    
+    $nextnext.style.filter = `blur(5px)`
   }
 
   function autoPlay(){
@@ -342,10 +383,20 @@ export default function Carousel(domElem, option){
   }
 
 
+  const $background = document.querySelector('.background');
+  $background.style.transform = 'scale(1.2)';
+
 function onBlur(){
   windowHasFocus = false;
   isPlaying = true;
   onAutoplayButton();
+
+  $magiccircle.style.animation = 'none';
+  for(let a of $robotImgs){
+    //로봇이미지 멈추게
+    // a.style.animation = 'none';
+  }
+
   console.log('lose focus');
 }
 
@@ -354,7 +405,23 @@ function onFocus(){
   isPlaying = false;
   clearInterval(interval);
   setTimeout(onAutoplayButton,0);
+
+  $magiccircle.style.animation = 'roll 90s linear infinite';
+  for(let a of $robotImgs){
+    //로봇이미지 멈추게
+    // a.style.animation = 'robots 1s ease-in infinite alternate';
+  }
+
   console.log('get focus');
+}
+
+
+
+function mouseMoving(e){
+  let valX = innerWidth/2 - e.pageX;
+  let valY = innerHeight/2 - e.pageY;
+
+  gsap.to( $background, {x: valX/50, y: valY/50, scale:1.2});
 }
 
   setTimeout(onAutoplayButton,1700);
@@ -373,5 +440,9 @@ function onFocus(){
   $slider.addEventListener('mousemove', (e) => onMousemove(e));
   
   window.addEventListener('blur',onBlur);
-  window.addEventListener('focus',onFocus);
+  window.addEventListener('focus',()=>{
+    onFocus();
+    // document.addEventListener('mousemove', (e) => mouseMoving(e));
+  });
+  document.addEventListener('mousemove', (e) => mouseMoving(e));
 }
